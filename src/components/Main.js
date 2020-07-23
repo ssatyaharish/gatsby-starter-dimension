adcfc9ecee5f7d4c4e54bdba60cfaf84b8fb6930
ssatyaharish <56021200+ssatyaharish@users.noreply.github.com>
2020-07-23 22:10:47 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Document, Page } from 'react-pdf'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import pdfFile from '../images/COVID-19-Guidelines.pdf'
 
 class Main extends React.Component {
   render() {
@@ -14,7 +16,9 @@ class Main extends React.Component {
         }}
       ></div>
     )
-
+    var elmnt = document.getElementById("main")
+    // var txt = elmnt.offsetHeight
+    console.log('this.props.setWrapperRef', window.screen.width)
     return (
       <div
         ref={this.props.setWrapperRef}
@@ -28,29 +32,24 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">SYMPTOMS</h2>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src="https://i.ytimg.com/vi/7zzfdYShvQU/maxresdefault.jpg" alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            Common signs of infection include respiratory symptoms, fever, cough,
+            shortness of breath and breathing difficulties. In more severe cases,
+            infection can cause pneumonia, severe acute respiratory syndrome,
+            kidney failure and even death(Dont worry the fatal cases are just 3%)
           </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+          <p>More than 80% of Corona positive
+            patients either have no symptoms
+            at all or show mild symptoms like
+            mild fever or cough. Such patients
+            do not need to get admitted into
+            hospital. They can stay home and
+            look after themselves. Please find quick links for home
+            isolation in the home page.
           </p>
           {close}
         </article>
@@ -62,25 +61,14 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Work</h2>
+          <h2 className="major">HOME-ISOLATION</h2>
           <span className="image main">
-            <img src={pic02} alt="" />
+            <Document file={pdfFile}>
+              <Page pageNumber={3} width={window.screen.width >= 768 ? 500 : null}/>
+              <Page pageNumber={4} width={window.screen.width >= 768 ? 500 : null}/>
+              <Page pageNumber={5} width={window.screen.width >= 768 ? 500 : null}/>
+            </Document>
           </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p>
           {close}
         </article>
 
@@ -91,19 +79,12 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">About</h2>
+          <h2 className="major">Diet</h2>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <Document file={pdfFile}>
+              <Page pageNumber={10} width={window.screen.width >= 768 ? 500 : null}/>
+            </Document>
           </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
           {close}
         </article>
 
@@ -115,53 +96,27 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
+          <p>
+            You can contact me for any other questions
+          </p>
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/ssharish"
                 className="icon fa-twitter"
+                target="_blank"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="https://www.facebook.com/ssatyaharish/"  target="_blank" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="https://www.instagram.com/harish_sankaramanchi"  target="_blank" className="icon fa-instagram">
                 <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
-                className="icon fa-github"
-              >
-                <span className="label">GitHub</span>
               </a>
             </li>
           </ul>
